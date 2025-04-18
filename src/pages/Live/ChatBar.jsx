e3f5d6d7f19c4439988de756c499d52ffaef0467
BEYUD <beyud@attcloud.org>
2025-04-18 07:55:57 +0700

    build 18/04

--- src/pages/Live/ChatBar.jsx
+++ src/pages/Live/ChatBar.jsx
@@ -66,7 +66,7 @@ export default function ChatBar({ ...rest }) {
   const [isSendCode, setIsSendCode] = useState(false);
   const [message, setMessage] = useState("");
   const [isEmoOpen, setIsEmoOpen] = useState(false);
-  const [allowChat, setAllowChat] = useState(true);
+  const [allowChat, setAllowChat] = useState(false);
 
   const { isAuthenticated } = useAuth();
   const {
