00e34e213ace216f3b58b6c1e1da78235b001f66
BEYUD <beyud@attcloud.org>
2025-04-24 10:21:10 +0700

    update/chat-interface: adjust chat responsiveness

--- src/pages/Live/Chat.jsx
+++ src/pages/Live/Chat.jsx
@@ -261,7 +261,7 @@ function ChatFrame({ ...rest }) {
   return (
     <div
       className={`text-white flex flex-col p-2 ${chatHeightSetting} overflow-auto ${
-        !isMobileKeyboardOpen ? "h-[40dvh]" : ""
+        !isMobileKeyboardOpen ? "h-[35dvh]" : ""
       }`}
       {...rest}
     >
